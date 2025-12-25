ed7c8bcdcb88290f084d7a980923d6b0d80d5ca6
Ayush Kewadia <ayush.kewadia@gmail.com>
2025-12-25 20:51:36 +0530

    DS

---
+++ 3/file.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int num;
+	for(int i = 1; true; i++){
+		cin >> num;
+		if(num == 42){
+			break;
+		}
+		cout<<num<<"\n";
+	}
+
+	return 0;
+}	
+
